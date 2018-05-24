3d1999a08fee5c756c4d356386a3d6af5874bd5d
jonsirrine <sir15004@byui.edu>
2018-05-24 16:57:39 -0700

    removed try catch

--- web/db_connect.php
+++ web/db_connect.php
@@ -2,28 +2,24 @@
 	function db_connect(){
 		$db = NULL:
 		
-		try {
-			$dbUrl = getenv('DATABASE_URL');
-				
-			if (!isset($dbUrl) || empty($dbUrl)){
-				echo "env variable null\n";
-				$dbUrl = ""; //local url (i.e. postgres://ta_user:ta_pass@localhost:5432/scripture_ta)
-			}
 
-			// setup db
-			$dbopts = parse_url($dbUrl);	
-			$dbHost = $dbopts["host"];
-			$dbPort = $dbopts["port"];
-			$dbUser = $dbopts["user"];
-			$dbPassword = $dbopts["pass"];
-			$dbName = ltrim($dbopts["path"],'/');
-			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$dbUrl = getenv('DATABASE_URL');
 			
-		} catch (PDOException $ex) {
-			echo "Error connecting to DB. Details: $ex";
-			die();
+		if (!isset($dbUrl) || empty($dbUrl)){
+			echo "env variable null\n";
+			$dbUrl = ""; //local url (i.e. postgres://ta_user:ta_pass@localhost:5432/scripture_ta)
 		}
+
+		// setup db
+		$dbopts = parse_url($dbUrl);	
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 		
 		return $db;
 	}
