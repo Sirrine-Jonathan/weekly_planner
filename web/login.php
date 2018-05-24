93dbac4876059c55533316149818806296dcc71f
jonsirrine <sir15004@byui.edu>
2018-05-24 16:36:40 -0700

    commented out, debugging

--- web/login.php
+++ web/login.php
@@ -16,6 +16,8 @@
 		$email = $_POST['email'];
 		$pwHash = password_hash($_POST['password'], PASSWORD_BCRYPT);
 		
+		echo $display . ' ' . $email . ' ' . $pwHash;
+		/*
 		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			if (isset($_POST['login'])) {
 				$user_id = loginUser($email, $pwHash, $display);
@@ -23,6 +25,8 @@
 				$user_id = registerUser($email, $pwHash, $display);
 			}
 		}
+		*/
+		
 	?>
 	<div class="header">
 		<h1>Welcome, </h1>
