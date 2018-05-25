6c984be26727bac01696e3d5737622cf497a5e04
jonsirrine <sir15004@byui.edu>
2018-05-24 19:41:00 -0700

    added querying

--- web/login.php
+++ web/login.php
@@ -34,8 +34,28 @@
 	<div class="header">
 		<h1>Welcome, <?php echo $display ?></h1>
 	</div>
+	<div>
+	<h3>User Preferences</h3>
+	<?php
+		//user_id here will be a session variable, hard coded as 1 for now
+		foreach ($db->query("SELECT * FROM user_preferences WHERE user_id='1'") as $row)
+		{
+			echo '<p>Dark Theme: ' . $row['dark_theme'] . '</p>';
+			echo '<p>Start on Mon: ' . $row['start_on_mon'] . '</p>';
+		}
+	?>
+	</div>
+	<div>
+	<h3>User Tasks</h3>
 	<?php
 	
+		foreach ($db->query("SELECT * FROM tasks WHERE user_id='1'") as $row)
+		{
+			echo '<p><b>' . $row['task_name'] . '</b></p>';
+			echo '<p>' . $row['due_date'] . '</p>';
+			echo '<p>' . $row['task_details'] . '</p>';
+		}
 	?>
+	</div>
 </body>
 </html>
