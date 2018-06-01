08cf6923fc4f1b607293cfae9cd1d00acfc3a64b
jonsirrine <sir15004@byui.edu>
2018-06-01 14:01:38 -0700

    trying to fix preferences page

--- web/preferences.php
+++ web/preferences.php
@@ -13,12 +13,13 @@
 	<?php
 		foreach ($db->query("SELECT * FROM user_preferences WHERE user_id='" . $_SESSION['user_id'] . "'") as $row)
 		{
-			$row['
+			$_SESSION['dark_theme'] = $row['dark_theme'];
+			$_SESSION['start_on_mon'] = $row['start_on_mon'];
 		}
 		//user_id here will be a session variable, hard coded as 1 for now
 		echo "<form method>";
 			echo '<p>Theme: '; 
-			$darkTheme = (($row['dark_theme']) ? true:false); 
+			$darkTheme = $_SESSION['dark_theme']; 
 			echo '<input type="radio" name="theme" value="dark" ';
 			if ($darkTheme) echo 'selected';
 			echo ' /';
@@ -28,7 +29,7 @@
 			'</p>';
 			
 			echo '<p>Start On: '; 
-			$startOnMon = (($row['start_on_mon']) ? true:false); 
+			$startOnMon = $_SESSION['start_on_mon']; 
 			echo '<input type="radio" name="start_day" value="mon" ';
 			if ($startOnMon) echo 'selected';
 			echo ' />';
