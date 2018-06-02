ff166c3368b1bfb3319177a7abc5c16e621058c2
jonsirrine <sir15004@byui.edu>
2018-06-02 05:02:05 -0700

    added class to login button

--- web/signupForm.php
+++ web/signupForm.php
@@ -4,7 +4,7 @@ echo "<form method='POST' action='login.php'>" .
 	"<h3>Log In / Sign In</h3>" .
 	"<input type='text' id='emailInput' name='email' placeholder='email@provider.com' /><br />" . 
 	"<input type='password' id='passwordInput' name='password' placeholder='password' /><br />" . 
-	"<input type='submit' name='login' value='Log In' />" . 
+	"<input type='submit' class='btn' name='login' value='Log In' />" . 
 	"<input type='submit' class='btn' name='register' value='Register' />" . 
 	"</form>";
 
